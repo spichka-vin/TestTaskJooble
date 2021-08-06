3c6995abff6bc86b8fcb7fee7684cb6af36dfeec
Alex Shevchuk <spichka.vin@gmail.com>
2021-08-06 14:49:16 +0300

    Add project files.

---
+++ TestTask/DeutschDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TestTask
+{
+    public class DeutschDictionary
+    {
+        public Dictionary<char, List<string>> Dictionary { get; set; }
+        private readonly string baseDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\");
+        private const string path = @"Data\de-dictionary.tsv";
+        public DeutschDictionary()
+        {
+            string fullPath = Path.Combine(baseDirectory, path);
+            if (File.Exists(fullPath))
+            {
+                Dictionary = new Dictionary<char, List<string>>();
+                foreach (string line in File.ReadAllLines(fullPath))
+                {
+                    AddWord(line);                
+                }
+            }
+            else
+            {
+                Console.WriteLine("File doesn`t exist!");
+            }
+        }
+        private void AddWord(string word)
+        {
+            string lowercaseWord = word.ToLower();
+            char key = lowercaseWord[0];
+            if (Dictionary.ContainsKey(key))
+            {
+                Dictionary[key].Add(lowercaseWord);
+            }
+            else
+            {
+                Dictionary.Add(key, new List<string>());
+                Dictionary[key].Add(lowercaseWord);
+            }
+        }
+
+
+
+    }
+}
---
+++ TestTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            DeutschDictionary dictionary = new DeutschDictionary();
+        }
+    }
+}
